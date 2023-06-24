12cb2b167d0f6fe400609eeeff754b910d2cac51
Gisele Macieira <giselefchaves@gmail.com>
2023-06-24 16:16:42 -0300

    recebimento de username por Header

--- src/app.js
+++ src/app.js
@@ -30,17 +30,18 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.post('/tweets', (req, res) => {
-    const {username, tweet} = req.body;
+    const {tweet} = req.body;
+    const {user} = req.headers
     if(usuarios.length === 0){
         res.status(401).send('UNAUTHORIZED');
         return;
     }
-    if(!username || !tweet || typeof(username) !== 'string' || typeof(tweet) !== 'string'){
+    if(!user || !tweet || typeof(user) !== 'string' || typeof(tweet) !== 'string'){
         res.status(400).send("Todos os campos são obrigatórios!");
         return;
     }
     const userTweet = {
-        username,
+        username: user,
         tweet,
         avatar: avatarUser
     }
